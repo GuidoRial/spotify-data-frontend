fcc1cca507e9043df2ce07dc92bb21149d821c77
GuidoRial <rialguido@gmail.com>
2023-03-11 13:40:19 -0300

    Handle refresh token

--- src/service/api.js
+++ src/service/api.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import auth from './auth';
+
+import useAuth from '../store/auth';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:3001/',
@@ -11,6 +14,31 @@ axiosInstance.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+let retries = 0;
+axiosInstance.interceptors.response.use(
+  async (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (!originalRequest._retry && error.response && error.response.status === 401 && retries < 3) {
+      originalRequest._retry = true;
+      retries++;
+
+      const authStore = useAuth();
+      const { accessToken } = await auth.refreshAccessToken({ refresh_token: authStore.refreshToken });
+      authStore.accessToken = accessToken;
+      window.localStorage.setItem('access-token', accessToken);
+      axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+
+      return axiosInstance(originalRequest);
+    }
+
+    retries = 0;
     return Promise.reject(error);
   }
 );
--- src/service/auth.js
+++ src/service/auth.js
@@ -4,4 +4,7 @@ export default {
   login(code) {
     return axios.post('http://localhost:3001/login', { code }).then((res) => res.data);
   },
+  refreshAccessToken(refresh_token) {
+    return axios.post('http://localhost:3001/refresh', refresh_token).then((res) => res.data);
+  },
 };
